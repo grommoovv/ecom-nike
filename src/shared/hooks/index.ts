d8ada7f2fd6879810ad6dd066d113ab04a4ad7f9
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-10-03 15:45:02 +0300

    hooks

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,3 +1,7 @@
-import { useAppDispatch, useAppSelector } from './redux/reduxHooks'
+import { useDispatch, TypedUseSelectorHook, useSelector } from 'react-redux'
+import { AppDispatch, AppState } from '../../store'
+
+const useAppDispatch = () => useDispatch<AppDispatch>()
+const useAppSelector: TypedUseSelectorHook<AppState> = useSelector
 
 export { useAppDispatch, useAppSelector }
